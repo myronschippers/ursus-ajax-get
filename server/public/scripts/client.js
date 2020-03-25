f4fe6c7214201e5944f2adba51437b284459d6bb
Myron <dubtwee@gmail.com>
2020-03-25 14:16:38 -0500

    styling and rendering all quotes to the page

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,11 +12,13 @@ function getQuotes() {
   // GET my quotes from the server
   $.ajax({// .ajax() returns a Promise
     method: 'GET',
-    url: '/all-quote',
+    url: '/all-quotes',
   })
   .then((response) => {
     console.log(response);
-    allQuotes = reponse;
+    allQuotes = response;
+
+    render();
   }) // handles a successful response
   .catch((err) => {
     console.log(err);
@@ -25,6 +27,18 @@ function getQuotes() {
 
 function render() {
   // appending my list to the DOM
+
+  for (let i = 0; i < allQuotes.length; i++) {
+    const quoteObject = allQuotes[i];
+    console.log(quoteObject);
+
+    $('.js-quotes').append(`
+      <div class="stackedQuote">
+        <p class="stackedQuote-quote">${quoteObject.quote}</p>
+        <em class="stackedQuote-author">by - ${quoteObject.author}</em>
+      </div>
+    `);
+  }
 }
 
 console.log('HEY URSUS');
